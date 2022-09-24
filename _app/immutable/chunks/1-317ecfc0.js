edd78c3d5a326ba7c2b5beaba484139c98efbe6c
realTristan <heytristaann@gmail.com>
2022-09-23 20:41:46 -0400

    Updates

--- _app/immutable/chunks/1-317ecfc0.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-3a4fa0f6.js";export{t as component};
---
+++ _app/immutable/chunks/1-5e1453fe.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-ed44c314.js";export{t as component};
