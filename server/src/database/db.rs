8796bef62048c1691b04fd7de5ece98f07da535a
tristan <heytristaann@gmail.com>
2022-09-24 16:09:31 -0400

    converted db to sqlx for thread safety

--- server/src/database/db.rs
+++ server/src/database/db.rs
@@ -1,17 +1,15 @@
-
 // Database Struct for globalizing it's
 // connection variable
-pub struct Database { conn: rusqlite::Connection }
+pub struct Database { conn: sqlx::SqlitePool }
 
 // Store The User data as a struct
 #[derive(Debug)]
-#[allow(dead_code)]
 pub struct User {
-    id:         i8,         // Row Increment ID
-    hash:       String,     // The user hash (aka: the user id)
-    name:       String,     // The user name
-    rsl:        i8,         // Whether the user has "Require Student Login" Enabled
-    analytics:  i8          // Whether the user has "Analytics" Enabled
+    pub id:             i64,        // Row Increment ID
+    pub hash:       String,     // The user hash (aka: the user id)
+    pub name:       String,     // The user name
+    pub rsl:        i64,        // Whether the user has "Require Student Login" Enabled
+    pub analytics:  i64         // Whether the user has "Analytics" Enabled
 }
 
 // Database Implemenetation that contains all the
@@ -19,69 +17,48 @@ pub struct User {
 #[allow(dead_code)]
 impl Database {
     // Initialize a new database connection
-    pub fn init() -> rusqlite::Result<Database> {
-        return Ok(Database{ conn: rusqlite::Connection::open_in_memory()? });
+    pub async fn init() -> Database {
+        // Initialize a connection to the sqlite database
+        return Database{ conn: sqlx::sqlite::SqlitePoolOptions::new()
+            .max_connections(5)
+            .connect_with(
+                sqlx::sqlite::SqliteConnectOptions::new()
+                    .filename("database.sqlite")
+                    .create_if_missing(true),
+            )
+            .await
+            .expect("Couldn't connect to database") }
     }
 
-    // The function to create the primary database table
-    // and add a test value into said table
-    pub fn establish_database(&self) -> rusqlite::Result<()> {
-        // Create New Database: (user_hash, user_name, require_student_login[bool], analytics[bool])
-        let _ = &self.conn.execute(
-            "CREATE TABLE users (id INTEGER PRIMARY KEY, user_hash TEXT NOT NULL, user_name TEXT NOT NULL, rsl INTEGER, analytics INTEGER)", 
-            rusqlite::params![]
-        )?;
-        // Insert test value into the database table
-        let _ = &self.conn.execute(
-            "INSERT INTO users (user_hash, user_name, rsl, analytics) VALUES (?1, ?2, ?3, ?4)",
-            rusqlite::params!["822f3d5b9c91b570a4f1848c5d147b4709d2fb96", "heytristaann", 0, 0]
-        )?;
-        return Ok(())
-    }
-
-    // The query_users() function is used to
-    // query all the users within the database and print
-    // their user_hashes to the screen. This function is primarily
-    // used for testing purposes.
-    pub fn query_users(&self) -> rusqlite::Result<Vec<User>> {
-        // Prepare the sqlite query command
-        let prep = &mut self.conn.prepare("SELECT id, user_hash, user_name, rsl, analytics FROM users")?;
-        // Iterate over the queried users
-        let r = prep.query_map([], |r| {
-            Ok(User {
-                id:         r.get(0)?,
-                hash:       r.get(1)?,
-                name:       r.get(2)?,
-                rsl:        r.get(3)?,
-                analytics:  r.get(4)?,
-            })
-        })?;
-        // Result array
-        let res = r.into_iter()
-            .map(|f| { f.unwrap()} )
-            .collect();
-
-        // Return success notifier
-        return Ok(res)
+    // The insert_test_user() function is used to
+    // insert a fake user for testing the backend
+    // database functions
+    pub async fn insert_test_user(&self) -> sqlx::sqlite::SqliteQueryResult {
+        return sqlx::query!( 
+            "INSERT INTO users (user_hash, user_name, rsl, analytics) VALUES (?, ?, ?, ?)",
+            "822f3d5b9c91b570a4f1848c5d147b4709d2fb96", "realtristan", 0, 0
+        ).execute(&self.conn).await.unwrap();
     }
 
     // The query_user_by_hash() function is used to query
     // the database for an user with the provided hash
     // Once found, the function will return the users
     // name, rsl(bool), analytics(bool) and id
-    pub fn query_user_by_hash(&self, user_hash: &str) -> rusqlite::Result<User> {
-        // Prepare the sqlite query command
-        let prep = &mut self.conn.prepare("SELECT id, user_name, rsl, analytics FROM users WHERE user_hash=:user_hash")?;
-        let r = prep.query_row(&[(":user_hash", user_hash)], |r| {
-            Ok(User {
-                id:         r.get(0)?,
-                hash:       user_hash.to_string(),
-                name:       r.get(1)?,
-                rsl:        r.get(2)?,
-                analytics:  r.get(3)?,
-            })
-        });
-        // Return the user
-        return Ok(r.unwrap())
+    pub async fn query_user_by_hash(&self, user_hash: &str) -> User {
+        // Query the database
+        let r = sqlx::query!(
+            "SELECT id, user_name, rsl, analytics FROM users WHERE user_hash=?",
+            user_hash
+        ).fetch_one(&self.conn).await.unwrap();
+
+        // Return the 'User' object containing all of
+        // the requested user's data
+        return User {
+            id: r.id,
+            hash: user_hash.to_string(),
+            name: r.user_name,
+            rsl: r.rsl,
+            analytics: r.analytics
+        }
     }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -2,30 +2,31 @@
 
 // Import the database functions
 #[path = "./database/db.rs"] mod sql;
-type DatabaseState = rocket::State<std::sync::Mutex<sql::Database>>;
-
+type Database = rocket::State<sql::Database>;
 
 // Get user by user_hash endpoint
 // /822f3d5b9c91b570a4f1848c5d147b4709d2fb96/no_auth
 #[get("/<user_hash>/<auth_token>")]
-pub fn verify(db: &DatabaseState, user_hash: &str, auth_token: &str) -> String {
-    // Lock the database state mutex
-    let db = db.lock().expect("shared state lock");
-    // Query the user
-    let r = db.query_user_by_hash(user_hash);
-    // Return the user_hash and auth_token
-    return format!("User Hash: {}\nAuth Token: {}\nUser Data: {:?}", user_hash, auth_token, r.unwrap())
+pub async fn verify(db: &Database, user_hash: &str, auth_token: &str) -> String {
+    let user: sql::User = db.query_user_by_hash(user_hash).await;
+
+    // Return a json map containing all the user data
+    return format!(
+        "{{\"auth_token\": \"{}\", \"user_hash\": \"{}\", \"user_name\": \"{}\", \"user_rsl\": {}, \"user_analytics\": {}}}", 
+            auth_token, user_hash, user.name, user.rsl==1, user.analytics==1
+    )
 }
+// https://github.com/diesel-rs/diesel
 
 // Launch Endpoints
 #[launch]
-fn rocket() -> _ {
+async fn rocket() -> _ {
     // Establish a new database
-    let db: sql::Database = sql::Database::init().unwrap();
-    let _ = db.establish_database();
+    let db: sql::Database = sql::Database::init().await;
+    let _ = db.insert_test_user().await;
 
     // Build the API Endpoints
     rocket::build()
-        .manage(std::sync::Mutex::new(db))
+        .manage(db)
         .mount("/", routes![verify])
 }
