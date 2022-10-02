59c0f6da70ee6392e34ef7e1de8a8b69d590969e
tristan <heytristaann@gmail.com>
2022-10-02 10:20:54 -0400

    cleaned up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,7 @@
 </svelte:head>
 
 <script>
-    import { onMount } from 'svelte'
-    import { fade } from 'svelte/transition'
-  import LoginMenu from './components/LoginMenu.svelte';
-    
-    // Track when the page loaded to use a
-    // fade-in animation
-    let pageLoaded = false
-    onMount(() => pageLoaded = true)
+    import LoginMenu from './components/LoginMenu.svelte';
 </script>
 
 <!-- h-screen bg-[url(images/bg.png)]-->
@@ -18,11 +11,9 @@
   class="bg-slate-800"
   style="background-size: cover; background-repeat: no-repeat; background-position: center center;"
 >
-    <main>{#if pageLoaded}
-        <div transition:fade="{{ duration: 700 }}">
-            <div class="flex justify-center items-center">
-                <LoginMenu/>
-            </div>
+    <main>
+        <div class="flex justify-center items-center">
+            <LoginMenu/>
         </div>
-    {/if}</main>
+    </main>
 </body>
