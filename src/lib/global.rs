347dfe9b655a65946bf98ab90424c673f7d068ac
Tristan <75189508+realTristan@users.noreply.github.com>
2022-12-17 13:48:24 -0500

    Update global.rs

--- src/lib/global.rs
+++ src/lib/global.rs
@@ -1,28 +1,12 @@
 use actix_web::HttpRequest;
 
 // The get_header() function is used to bypass
-// any invalid header errors. Without this function,
-// if an abuser tried to send a request with an
-// invalid error, an internal server error would occur.
-pub fn get_header<'a>(req: &'a HttpRequest, key: &str) -> &'a str {
-    // Get the header option to check whether the
-    // head is valid/present
-    let opt_head = req.headers().get(key);
-    // If the header is invalid/not-present, return
-    // an empty string
-    if opt_head.is_none() {
-        return "";
-    }
-    // Unwrap the option header and check if
-    // it has a valid length.
-    let head_val = opt_head.unwrap();
-    // If it doesn't, return an empty string
-    if head_val.is_empty() {
-        return "";
-    }
-    // Finally return the header as an
-    // unwrapped string
-    return head_val.to_str().unwrap();
+// any invalid header errors.
+pub fn get_header(req: &HttpRequest, key: &str) -> String {
+    return match req.headers().get(key) {
+        Some(v) => v,
+        None => return "".to_string(),
+    }.to_str().unwrap().to_string();
 }
 
 // The get_time() function is used to quickly
